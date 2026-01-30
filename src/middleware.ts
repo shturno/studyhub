4293d665772edd533ffb15457ab9467d0b106905
kai <kai.s.alvarenga@gmail.com>
2026-01-30 15:23:46 -0300

    refactor: migrate authentication and authorization middleware logic to `auth.config.ts`'s `authorized` callback.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,28 +1,7 @@
 import NextAuth from "next-auth"
 import { authConfig } from "@/lib/auth.config"
-import type { NextRequest } from "next/server"
 
-import { Session } from "next-auth"
-
-interface NextAuthRequest extends NextRequest {
-    auth: Session | null
-}
-
-const { auth } = NextAuth(authConfig)
-
-export default auth(async (req) => {
-    const isLoggedIn = !!req.auth
-    const isOnDashboard = req.nextUrl.pathname.startsWith('/dashboard')
-    const isAuthPage = req.nextUrl.pathname.startsWith('/login') || req.nextUrl.pathname.startsWith('/register')
-
-    if (isOnDashboard && !isLoggedIn) {
-        return Response.redirect(new URL('/login', req.nextUrl))
-    }
-
-    if (isAuthPage && isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', req.nextUrl))
-    }
-})
+export default NextAuth(authConfig).auth
 
 export const config = {
     matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
